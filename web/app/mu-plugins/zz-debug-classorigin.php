275d4f8e68c9c89a800a05bd0147e19f948ed9a9
Johan Malm <johanmalm@MacBook-Air-som-tillhor-Johan.local>
2026-06-05 14:28:16 +0200

    Stadning: debug-mu-plugin bort, force-sync permanentad, debug-flaggor av

--- web/app/mu-plugins/zz-debug-classorigin.php
+++
@@ -1,9 +0,0 @@
-<?php
-add_action('plugins_loaded', function() {
-    if (class_exists('FasadBridge\FasadBridge', false)) {
-        $r = new ReflectionClass('FasadBridge\FasadBridge');
-        error_log('FASADBRIDGE REDAN LADDAD FRAN: ' . $r->getFileName());
-    } else {
-        error_log('FASADBRIDGE EJ LADDAD vid plugins_loaded');
-    }
-}, -9999);

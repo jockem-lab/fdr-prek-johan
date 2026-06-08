673a4b2030ead5812480805eba3a8ecd1deac4e6
Johan Malm <jockem@gmail.com>
2026-06-08 12:34:05 +0200

    Heror full-bleed: bort ur 1440-lagret + em-detalj-hero padding 0 (gick ej till kant pa start/objektsida)

--- web/app/themes/standardsite/resources/views/partials/content-single-fasad_listing.blade.php
+++ web/app/themes/standardsite/resources/views/partials/content-single-fasad_listing.blade.php
@@ -1,5 +1,5 @@
 @if(class_exists('PrekWeb\\Includes\\Fasad') && PrekWeb\Includes\Fasad::imageRoute())
   <?php echo('<pre>'.print_r('image route', true).'</pre>'); ?>
 @else
-  @include('fasad.content-single-fasad_listing')
+  @include('partials.content-fasad_listing')
 @endif
